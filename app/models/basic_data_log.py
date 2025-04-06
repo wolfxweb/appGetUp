e05b88d277e7317f74720d3939827ea7afe112bc
Eduardo lobo <63437120+wolfxweb@users.noreply.github.com>
2025-04-06 13:07:42 -0300

    dados

--- app/models/basic_data_log.py
+++ app/models/basic_data_log.py
@@ -1,6 +1,7 @@
 # app/models/basic_data_log.py
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
+from datetime import datetime
 from app.database.db import Base
 
 class BasicDataLog(Base):
@@ -9,5 +10,6 @@ class BasicDataLog(Base):
     id = Column(Integer, primary_key=True, index=True)
     basic_data_id = Column(Integer, ForeignKey('basic_data.id', ondelete="CASCADE"))
     change_description = Column(String)
+    created_at = Column(DateTime, default=datetime.now)
     
     basic_data = relationship("BasicData", back_populates="logs")
