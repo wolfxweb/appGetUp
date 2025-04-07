1a35581cfb626d0f7e2d8eec13c0cdcd08db2f86
Eduardo lobo <63437120+wolfxweb@users.noreply.github.com>
2025-04-07 19:41:55 -0300

    data

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -11,6 +11,7 @@ from app.routes.basic_data_operations import router as basic_data_operations_rou
 from app.routes.priorities import router as priorities_router
 from app.routes.calculator import router as calculator_router
 from app.routes.diagnostico import router as diagnostico_router
+from app.routes.profile import router as profile_router
 
 router = APIRouter()
 
@@ -19,4 +20,5 @@ router.include_router(basic_data_router)
 router.include_router(basic_data_operations_router)
 router.include_router(priorities_router)
 router.include_router(calculator_router)
-router.include_router(diagnostico_router) 
+router.include_router(diagnostico_router)
+router.include_router(profile_router) 
